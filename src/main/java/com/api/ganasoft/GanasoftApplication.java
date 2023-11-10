e4e858d0a61a72a217400066e9a0ebb217dea590
Kevin Rincon <estebanrincon0926@gmail.com>
2023-11-10 16:18:03 -0500

    prueba

--- src/main/java/com/api/ganasoft/GanasoftApplication.java
+++ src/main/java/com/api/ganasoft/GanasoftApplication.java
@@ -2,9 +2,17 @@ package com.api.ganasoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GanasoftApplication {
+
+	@GetMapping("/mensaje")
+	public String message (){
+		return "Funciono :)";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GanasoftApplication.class, args);
